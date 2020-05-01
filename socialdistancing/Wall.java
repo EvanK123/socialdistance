2588d6de428992d241bf8b5a59c8306eead7c060
EvanK123 <42948168+EvanK123@users.noreply.github.com>
2020-05-01 13:13:00 -0700

    removing walls from walls class

--- socialdistancing/Wall.java
+++ socialdistancing/Wall.java
@@ -5,7 +5,8 @@ import java.awt.Rectangle;
 
 import javax.swing.ImageIcon;
 
-public class Wall{
+public class Wall
+{
 
     protected int x;
     protected int y;
@@ -17,6 +18,7 @@ public class Wall{
 
     public Wall(int x, int y, String imageS, boolean vertical) 
     {
+
         this.x = x;
         this.y = y;
         visible = true;
@@ -27,6 +29,7 @@ public class Wall{
 
     protected void loadImage(String imageName) 
     {
+
         ImageIcon ii = new ImageIcon(imageName);
         image = ii.getImage();
     }
